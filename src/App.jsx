b1d5a6058d1e6a4e8ecc73424707f9f4e0573c5f
Salomao-Faleta <faletas41@gmail.com>
2024-02-01 08:08:59 -0300

    finalizando o proejto
    fazendo a  troca dos pedidos com states =)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import Restaurante from './assets/hashtaurante.webp'
+import { useState } from 'react';
 import './App.css';
+import Restaurante from './assets/hashtaurante.webp'
 import { Navegacao } from './components/navegacao';
+import { ItemCardapio } from './components/ItemCardapio';
+import { pratosPrincipais, sobremesas, bebidas } from './cardapio';
+
+//variavel de controle
+
 
 function App() {
+    const paginasMenu = [pratosPrincipais, sobremesas, bebidas]
+    const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
     return(
-        <div>
+        <>
             <img src={Restaurante} alt="Foto do local" className='capa'/>
-            <Navegacao/>
-        </div>
+            <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+            
+            <div className='menu'>
+                {paginasMenu[paginaSelecionada].map(item => 
+                <ItemCardapio 
+                    nome={item.nome} 
+                    descricao={item.descricao} 
+                    preco={item.preco}  
+                    imagem={item.imagem}  
+                />)}
+            </div>
+        </>
     );
 }
 
